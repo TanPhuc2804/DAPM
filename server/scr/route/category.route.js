f318593e27958be7c3dce1bae4a508c4550cfcfc
tothaihao <thaihaoto@gmail.com>
2024-10-11 10:10:03 +0700

    Add files via upload

---
+++ server/scr/route/category.route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {
+    createCategory,
+    getAllCategories,
+    getCategoryById,
+    updateCategory,
+    deleteCategory
+} = require('../controller/category.controller');
+
+const categoryRouter = express.Router();
+
+categoryRouter.post('/', createCategory);
+
+categoryRouter.get('/', getAllCategories);
+
+categoryRouter.get('/:id', getCategoryById);
+
+categoryRouter.put('/:id', updateCategory);
+
+categoryRouter.delete('/:id', deleteCategory);
+
+module.exports = categoryRouter;
