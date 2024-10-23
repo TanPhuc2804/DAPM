4e3ad149543e553833cf8daf210408e58e223c11
Tan Phuc <phantanphuc282004@gmail.com>
2024-10-23 21:30:27 +0700

    fix image

--- client_user/src/pages/user/ProductCard/index.jsx
+++ client_user/src/pages/user/ProductCard/index.jsx
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { Link } from "react-router-dom";
 const ProductCard = ({ _id,image, name, price, originalPrice, discount, description }) => {
-  console.log(image)
   return (
     <div className="flex flex-col w-full rounded-1xl max-md:max-w-full overflow-hidden group max-w-[380px] mx-4 my-2 transition-all duration-300 hover:shadow-lg hover:border hover:border-black"> {/* Thêm hiệu ứng hover */}
       <div className="flex flex-col items-center px-1 pt-2 pb-4 w-full bg-white rounded-2xl">
         <div className="flex relative flex-col items-center self-stretch px-2 pt-3 text-2xl font-extralight leading-none text-black whitespace-nowrap bg-blend-darken min-h-[400px] pb-4 max-md:pr-5 max-md:pb-16">
           <img
             loading="lazy"
-            src={image}
+            src={image[0]}
             alt={name}
             className="object-cover w-full h-full transition duration-300 group-hover:opacity-75"
           />
